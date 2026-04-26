5ea5af031d4b410b2ae833ca9d740f70b1a9ec33
vik-kalsi <107356027+vik-kalsi@users.noreply.github.com>
2026-04-26 18:45:00 +0100

    Designing mobile registration page

--- resources/views/pages/registration.blade.php
+++ resources/views/pages/registration.blade.php
@@ -6,8 +6,23 @@
     </x-slot>
 
 
-    <div class="mt-4 font-bold">
-        <p>This is the registration page</p>
+    <div class="grid justify-center mt-4 font-bold">
+        <h1 class="mb-4 text-2xl">Please register here</h1>
+
+
+        <form class="grid gap-3 w-30" action="/register" method="post">
+            @csrf
+            <input class="border-2 p-1" type="email" name="email" placeholder="Email Address" value="{{ old('email') }}">
+
+            <input class="border-2 p-1" type="text" name="name" placeholder="Name" value="{{ old('name') }}">
+
+            <input class="border-2 p-1" type="password" name="password" placeholder="Password">
+
+            <input class="border-2 p-1" type="password" name="password_confirmation" placeholder="Confirm Password">
+
+            <button class="border-2 p-1 cursor-pointer font-bold" type="submit">Register</button>
+
+        </form>
     </div>
 
 
