e2f530695573a83fb5e5fb5b67bf812307a072da
vik-kalsi <107356027+vik-kalsi@users.noreply.github.com>
2026-05-11 20:52:35 +0100

    Finishing on expiry and expired documents

--- app/Http/Controllers/ExpiringDocumentsController.php
+++ app/Http/Controllers/ExpiringDocumentsController.php
@@ -6,28 +6,16 @@ use Illuminate\Http\Request;
 
 use Carbon\Carbon;
 
-use App\Models\Document;
-
-
 
 class ExpiringDocumentsController extends Controller
 {
     public function OpenExpiringDocumentPage()
     {
-        $document = Document::find(37);
+        $documents = auth()->user()
+            ->documents()
+            ->whereDate('expiry_date', '<=', Carbon::now()->addDays(30))
+            ->get();
 
-        $expiryDate = Carbon::parse($document->expiry_date);
-        $today = Carbon::today();
-
-        // Days remaining
-        $daysLeft = $today->diffInDays($expiryDate, false);
-
-        // Check if under reminder days
-        $isUnderReminder = $daysLeft <= $document->reminder_days;
-
-        //return view('pages.expiringdocuments');
-        
-        //return $daysLeft . " days";
-        return $isUnderReminder;
+        return view('pages.expiringdocuments', compact('documents'));   
     }
 }
--- resources/views/pages/expiringdocuments.blade.php
+++ resources/views/pages/expiringdocuments.blade.php
@@ -5,10 +5,23 @@
     </x-slot>
 
 
-    <h1 class="font-bold text-4xl">Expiring Documents</h1>
+    <h1 class="font-bold text-4xl">Expiring or Expired Documents</h1>
 
 
     <div class="mt-4">
+
+        <p>These are the documents that have already expired or will expire within 30 days</p>
+
+        @foreach ($documents as $document)
+            <ul class="grid justify-center">
+                <li class="border-4 my-4 p-3 w-xs">
+                    <div>
+                        <p>Document Name: {{ $document->name }}</p>
+                        <p>Expiry Date: {{ $document->expiry_date }}</p>
+                    </div>
+                </li>
+            </ul>
+        @endforeach
         
     </div>
 
